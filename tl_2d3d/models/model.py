4867e74ac5037ec4ee3cc26c33ab7399381b8edf
SebastianBitsch <sebastianbitsch@protonmail.com>
2024-04-22 13:21:49 +0200

    load weights

--- tl_2d3d/models/model.py
+++ tl_2d3d/models/model.py
@@ -1,15 +1,23 @@
+import torch
 import torch.nn as nn
 from monai.networks.nets import DynUNet
 from omegaconf import DictConfig
 
 def make_model(config: DictConfig, device: str) -> nn.Module:
     model = DynUNet(
-        spatial_dims = 2,   # 2 for 2D convolutions, 3 for 3D convolutions
-        in_channels  = 1,   # Number of input channels/modalities (3 for RGB)
-        out_channels = config.data.n_classes,   # Number of classes, including background
-        kernel_size  = [3, 3, 3, 3, 3, 3], # Size of the filters
-        strides      = [1, 2, 2, 2, 2, 2],
-        upsample_kernel_size = [2, 2, 2, 2, 2]
+        spatial_dims = config.model.num_dimensions,   # 2 for 2D convolutions, 3 for 3D convolutions
+        in_channels  = 1,   # cant be bothered to put a config var for this
+        out_channels = config.data.n_classes,
+        kernel_size  = config.model.kernel_size,
+        strides      = config.model.strides, 
+        upsample_kernel_size = config.model.upsample_kernel_size
     )
+
+    # Load weights
+    if config.model.path_to_weights:
+        print(f"Loading weights ({config.model.path_to_weights}) on to model")
+        model.load_state_dict(torch.load(config.model.path_to_weights))
+    else:
+        print(f"No weights loaded, training from scratch")
 
     return model.to(device)
