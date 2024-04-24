48898eb891bc5088b74af36b73d7916500ed7b2e
SebastianBitsch <sebastianbitsch@protonmail.com>
2024-04-24 21:51:51 +0200

    init weights function

--- tl_2d3d/utils.py
+++ tl_2d3d/utils.py
@@ -3,6 +3,7 @@ import random
 import torch
 import monai
 import numpy as np
+import torch.nn as nn
 
 
 def get_device(verbose: bool = True) -> str:
@@ -25,3 +26,31 @@ def set_seed(seed: int) -> None:
 def count_parameters(model): 
     """ Get the number of params in a model. See: https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325"""
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def init_normal_per_module(from_module: nn.Module, to_module: nn.Module) -> None:
+    """
+    Initialize the weights of a model (to_module) using the normal distributions of the layers in another module (from_module).
+    The function will take the weights in an entire module i.e. shape [128, 64, 3, 3] and compute a single number mean and std to 
+    init the entire layer in another module.
+    
+    Args:
+        from_module (nn.Module): Source module providing weights for initialization.
+        to_module (nn.Module): Target module whose weights will be initialized.
+    
+    Note:
+        The modules should be -very- similar as this functions makes the assumption the models have the same layers (either 2D or 3D) in the same order.
+    """
+    assert len(list(from_module.modules())) == len(list(to_module.modules())), "Error: Models should contain the 'same' layers"
+
+    for from_mod, to_mod in zip(from_module.modules(), to_module.modules()):
+
+        if isinstance(from_mod, (nn.Conv2d, nn.InstanceNorm2d, nn.ConvTranspose2d)):
+            # Get distributions across the entire module, i.e. a single number for mean and std
+            mean = torch.mean(from_mod.weight.data) 
+            std  = torch.std(from_mod.weight.data)
+            
+            to_mod.weight.data.normal_(mean, std)
+
+        elif isinstance(from_mod, nn.LeakyReLU):
+            to_mod.negative_slope = from_mod.negative_slope
